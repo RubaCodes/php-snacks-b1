07bc2ea948498121abeecce97c769cceca6f0c4c
rubacodes <rubacodes@gmail.com>
2022-06-22 14:20:04 +0200

    aggiunto style

--- snack-1/index.php
+++ snack-1/index.php
@@ -40,15 +40,19 @@ $partite =[
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
     <title>Document</title>
 </head>
-<body>
-    <h1>Risultati giornata odierna:</h1>
-    <ul>
-        <?php for($i =0 ; $i < count($partite); $i++) {?>
-        <li><?php echo "{$partite[$i]["homeTeam"]} - {$partite[$i]["awayTeam"]} || {$partite[$i]["homeScore"]}-{$partite[$i]["awayScore"]}"?></li>
-        <?php } ?>
-    </ul>
+<body class="grid place-items-center h-[100vh] bg-blue-500">
+    <div class="bg-blue-800 rounded-xl p-8 text-white border-4 border-white/75">
+        <h1 class="text-3xl pb-4">Risultati giornata odierna:</h1>
+        <ul class=" divide-y">
+            <?php for($i =0 ; $i < count($partite); $i++) {?>
+            <li class="py-2"><?php echo "{$partite[$i]["homeTeam"]} - {$partite[$i]["awayTeam"]} || {$partite[$i]["homeScore"]}-{$partite[$i]["awayScore"]}"?></li>
+            <?php } ?>
+        </ul>
+    </div>
+    
 
     <!-- sezione dump data -->
     <!-- <pre><//?php var_dump($partite)?></pre> -->
