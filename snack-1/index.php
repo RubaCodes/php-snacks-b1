0f10ab8505fb87a9f8785b0f0f16820fbeffe540
rubacodes <rubacodes@gmail.com>
2022-06-22 14:12:54 +0200

    print partite corretto

--- snack-1/index.php
+++ snack-1/index.php
@@ -9,26 +9,26 @@ $partite =[
     [
         'homeTeam' => 'Siena',
         'awayTeam' => 'Treviso',
-        'homeScore' => rand(1,100),
-        'awayScore' => rand(1,100),
+        'homeScore' => rand(50,100),
+        'awayScore' => rand(50,100),
     ],
     [
         'homeTeam' => 'Napoli',
         'awayTeam' => 'Benevento',
-        'homeScore' => rand(1,100),
-        'awayScore' => rand(1,100),
+        'homeScore' => rand(50,100),
+        'awayScore' => rand(50,100),
     ],
     [
         'homeTeam' => 'Catania',
         'awayTeam' => 'Torino',
-        'homeScore' => rand(1,100),
-        'awayScore' => rand(1,100),
+        'homeScore' => rand(50,100),
+        'awayScore' => rand(50,100),
     ],
     [
         'homeTeam' => 'Venezia',
         'awayTeam' => 'Potenza',
-        'homeScore' => rand(1,100),
-        'awayScore' => rand(1,100),
+        'homeScore' => rand(50,100),
+        'awayScore' => rand(50,100),
     ],
 ];
 
@@ -43,8 +43,14 @@ $partite =[
     <title>Document</title>
 </head>
 <body>
+    <h1>Risultati giornata odierna:</h1>
+    <ul>
+        <?php for($i =0 ; $i < count($partite); $i++) {?>
+        <li><?php echo "{$partite[$i]["homeTeam"]} - {$partite[$i]["awayTeam"]} || {$partite[$i]["homeScore"]}-{$partite[$i]["awayScore"]}"?></li>
+        <?php } ?>
+    </ul>
 
     <!-- sezione dump data -->
-    <pre><?php var_dump($partite)?></pre>
+    <!-- <pre><//?php var_dump($partite)?></pre> -->
 </body>
 </html>
